f53dc47a17823bdd4e7bda4d2559d9c049e720a2
gregariousgovind <48236750+gregariousgovind@users.noreply.github.com>
2023-02-02 14:16:31 +0530

    Search implemented

--- src/app/components/country-cards/country-cards.component.ts
+++ src/app/components/country-cards/country-cards.component.ts
@@ -8,12 +8,23 @@ import { Component, Input } from '@angular/core';
 })
 export class CountryCards {
   public cards: any;
+  searchTerm: string;
+  filteredCards: any;
 
   constructor(private http: HttpClient) {}
 
   ngOnInit() {
     this.http.get('https://restcountries.com/v3.1/all').subscribe((res) => {
       this.cards = res;
+      this.filteredCards = this.cards;
+    });
+  }
+
+  search() {
+    this.filteredCards = this.cards.filter((card) => {
+      return card.name.common
+        .toLowerCase()
+        .includes(this.searchTerm.toLowerCase());
     });
   }
 }
